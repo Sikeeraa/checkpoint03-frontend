98b5211efdedb81e4eebde7685dba9df1e9adc94
Sikera <matheussequeirafranco@gmail.com>
2026-05-23 00:13:48 -0300

    chore: criando a estrutura do site com o html

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+console.log("NEON FORCE carregado!");
+
+window.addEventListener("load", () => {
+
+    const hero = document.getElementById("inicio");
+
+    setTimeout(() => {
+
+        hero.style.display = "none";
+
+    }, 4500);
+
+});
